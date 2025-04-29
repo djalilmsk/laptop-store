2c9b9d204a18132368e7975b2f665c1a30e75f17
Abd Eldjallil Meskali <155311349+djalilmsk@users.noreply.github.com>
2025-04-29 23:31:04 +0100

    fixing err

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -4,33 +4,32 @@ import { Toaster } from "@/components/ui/toaster";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import NavBar from "@/components/navbar/navigation-bar";
-import Footer from "@/components/footer/Footer";
+import Footer from "@/components/footer/footer";
 
 gsap.registerPlugin(useGSAP);
 
 function HomeLayout() {
   useGSAP(() => {
     const tl = gsap.timeline({ delay: 1 });
-    tl
-      .from(
-        ".nav",
-        {
-          y: -50,
-          stagger: 0.05,
-          ease: "power2.out",
-        },
-        "-=1",
-      );
+    tl.from(
+      ".nav",
+      {
+        y: -50,
+        stagger: 0.05,
+        ease: "power2.out",
+      },
+      "-=1",
+    );
   });
 
   return (
-    <div className="home-layout flex min-h-[100dvh] flex-col justify-between relative">
-      <div className="container mx-auto mb-10 px-4 sm:px-6 lg:px-8 ">
+    <div className="home-layout relative flex min-h-[100dvh] flex-col justify-between">
+      <div className="container mx-auto mb-10 px-4 sm:px-6 lg:px-8">
         <NavBar />
         <main>
           <Outlet />
         </main>
-        <Toaster  />
+        <Toaster />
       </div>
       <Footer />
     </div>
